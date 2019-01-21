1ac01dd83aa830dc623008dbbcc3f9ce297b21ac
huyvin <hc.vincent@gmail.com>
2019-01-21 19:20:22 +0100

    .

---
+++ views/footer.php
@@ -0,0 +1,91 @@
+    
+    <footer class="footer" style="background-color: #152f59;">
+
+    <div class="container">
+      <p>
+        &copy; huyvin
+      </p>
+    </div>
+      
+    </footer>
+    
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+    <script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+
+
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="card-header" style="background-color: #152f59;">
+            <div class="modal-header">
+              <h5 class="modal-title text-light" id="loginModalTitle">Connexion</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+          </div>
+        </div>
+          
+          <div class="modal-body">
+          <form>
+            <input type="hidden" id="loginActive" name="loginActive" value="1">
+            <div class="form-group">
+              <label for="email">Adresse Mail</label>
+              <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Entrez votre adresse mail">
+            </div>
+            <div class="form-group">
+              <label for="password">Mot De Passe</label>
+              <input type="password" class="form-control" id="password" placeholder="Entrez votre mot de passe">
+            </div>
+          </form>
+          </div>
+          <div class="modal-footer">
+            <a id="toggleLogin">S'enregistrer</a>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+            <button type="button" id="loginSignupButton" class="btn btn-primary" >Se Connecter</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script>
+      $("#toggleLogin").click(function() {
+        
+        if ($("#loginActive").val() == "1") {
+            
+            $("#loginActive").val("0");
+            $("#loginModalTitle").html("S'enregistrer");
+            $("#loginSignupButton").html("S'enregistrer");
+            $("#toggleLogin").html("Se Connecter");
+            
+            
+        } else {
+            
+            $("#loginActive").val("1");
+            $("#loginModalTitle").html("Se Connecter");
+            $("#loginSignupButton").html("Se Connecter");
+            $("#toggleLogin").html("S'enregistrer");
+            
+        }
+      });
+
+      $("#loginSignupButton").click(function() {
+        
+        $.ajax({
+          type: "POST",
+          url: "actions.php?action=loginSignup",
+          data: "email="+$("#email").val()+"&password="+$("#password").val()+"&loginActive="+$("#loginActive").val(),
+          success: function(result) {
+            alert(result);
+          }
+        });
+      });
+
+    </script>
+
+
+  </body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+    <!-- Material Design for Bootstrap fonts and icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+
+    <!-- Material Design for Bootstrap CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./styles.css">          
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #152f59;">
+      <a class="navbar-brand" href="/">TwitClone</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="?page=timeline">Votre fil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?page=messages">Vos messages</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?page=profile">Profil</a>
+          </li>
+        </ul>
+        <div class="form-inline my-2 my-lg-0">
+          <button class="btn btn-outline-info my-2 my-sm-0" data-toggle="modal" data-target="#exampleModal">Connexion / S'enregistrer</button>
+        </div>
+      </div>
+    </nav>
